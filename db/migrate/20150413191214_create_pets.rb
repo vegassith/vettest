dd720957d2c265523a1d4d99b2999192bf7fedf5
vegassith <markdmurphy@ymail.com>
2015-04-14 22:32:35 -0700

    Update 20150413191214_create_pets.rb

--- db/migrate/20150413191214_create_pets.rb
+++ db/migrate/20150413191214_create_pets.rb
@@ -2,7 +2,6 @@ class CreatePets < ActiveRecord::Migration
   def change
     create_table :pets do |t|
       t.string :name, :limit => 35, null: false
-      t.references :customer, index: true
       t.references :typeofpet, index: true
       t.string :breed,  :limit => 35,  null: false
       t.integer :age, null: false
